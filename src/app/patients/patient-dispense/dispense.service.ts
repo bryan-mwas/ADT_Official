5604ff13416c88d020c1bc85332f66537f3f9e6f
bryan-mwas <mwathibrian7@gmail.com>
2017-03-03 13:06:14 +0300

    Dispense Urls Removed preceeding /

--- src/app/patients/patient-dispense/dispense.service.ts
+++ src/app/patients/patient-dispense/dispense.service.ts
@@ -20,37 +20,37 @@ export class DispenseService {
      * GET
      */
     getDrugDetails(id: number): Observable<any> {
-    return this._http.get(this._url + `/drugs/${id}`)
+    return this._http.get(this._url + `drugs/${id}`)
       .map((response: Response) => <any[]>response.json())
       .do(data => console.log('IndividualDrugDetails: ' + JSON.stringify(data)))
       .catch(this.handleError);
   }
     getRegimens() {
-        return this._http.get(this._url + '/regimen')
+        return this._http.get(this._url + 'regimen')
             .map((res: Response) => <any[]>res.json()) // TODO: refactor this. Bad practice
             .do(data => console.log('Change Reasons: ' + JSON.stringify(data)))
             .catch(this.handleError);
     }
     getChangeReason() {
-        return this._http.get(this._url + '/lists/changereason')
+        return this._http.get(this._url + 'lists/changereason')
             .map((res: Response) => <any[]>res.json()) // TODO: refactor this. Bad practice
             .do(data => console.log('Change Reasons: ' + JSON.stringify(data)))
             .catch(this.handleError);
     }
     getPurpose() {
-        return this._http.get(this._url + '/lists/purpose')
+        return this._http.get(this._url + 'lists/purpose')
             .map((res: Response) => <any[]>res.json()) // TODO: refactor this. Bad practice
             .do(data => console.log('Purpose: ' + JSON.stringify(data)))
             .catch(this.handleError);
     }
     getNonAdherence() {
-        return this._http.get(this._url + '/lists/nonaadherencereason')
+        return this._http.get(this._url + 'lists/nonaadherencereason')
             .map((res: Response) => <any[]>res.json()) // TODO: refactor this. Bad practice
             .do(data => console.log('Non Adherance: ' + JSON.stringify(data)))
             .catch(this.handleError);
     }
     getRegimenDrugs(id: number) {
-        return this._http.get(this._url + `/regimen/${id}/drugs`)
+        return this._http.get(this._url + `regimen/${id}/drugs`)
             .map((res: Response) => <any[]>res.json()) // TODO: refactor this. Bad practice
             .do(data => console.log('Regimen Drugs: ' + JSON.stringify(data)))
             .catch(this.handleError);
@@ -61,7 +61,7 @@ export class DispenseService {
      * @param drug_id 
      */
     getDrugBatch(store_id: number, drug_id: number) {
-        return this._http.get(this._url + `/stock/store/${store_id}/drug/${drug_id}`)
+        return this._http.get(this._url + `stock/store/${store_id}/drug/${drug_id}`)
             .map((res: Response) => <any[]>res.json()) // TODO: refactor this. Bad practice
             .do(data => console.log('Batch: ' + JSON.stringify(data)))
             .catch(this.handleError);
@@ -91,7 +91,7 @@ export class DispenseService {
         let headers = new Headers({ 'Content-Type': 'application/json; charset=utf-8' }); // ... Set content type to JSON
         let options = new RequestOptions({ headers: headers }); // Create a request option
 
-        return this._http.post(`${this._url}/${body['patient_id']}/dispense`, body, options) // ...using put request
+        return this._http.post(`${this._url}${body['patient_id']}/dispense`, body, options) // ...using put request
             .map(() => body)
             .catch((error: any) => Observable.throw(error.json().error || 'Server error')); //...errors if a
     }
