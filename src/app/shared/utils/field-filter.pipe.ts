073d177675b80f27b7ae6e01e5c60499b912026d
bryan-mwas <mwathibrian7@gmail.com>
2017-03-05 12:28:48 +0300

    Multicolumn Search Functionality: Patient Listing

--- src/app/shared/utils/field-filter.pipe.ts
+++ src/app/shared/utils/field-filter.pipe.ts
@@ -5,9 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FieldFilterPipe implements PipeTransform {
 
-  transform(items: any[], field : string, value : string): any[] {
+  transform(items: any[], field: string, value: string): any[] {
     if (!items) return [];
     if (!value) return items;
+    // Filter all the fields
+    if (field == 'All') {
+      return items.filter(it => 
+        it['ccc_number'].toString().toLowerCase().indexOf(value.toLowerCase().toString()) > -1 ||
+        it['first_name'].toString().toLowerCase().indexOf(value.toLowerCase().toString()) > -1 ||
+        it['phone_number'].toString().toLowerCase().indexOf(value.toLowerCase().toString()) > -1
+        // it['current_status_name'].toString().toLowerCase().indexOf(value.toLowerCase().toString()) > -1
+        );
+    }
     return items.filter(it => it[field].toString().toLowerCase().indexOf(value.toLowerCase().toString()) > -1);
   }
 }
